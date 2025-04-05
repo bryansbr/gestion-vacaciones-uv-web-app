70cb8687846b233332d3cdd560c9ad8170200468
Bryan Biojó <bryan.biojo@correounivalle.edu.co>
2025-04-05 13:56:22 -0500

    feat: Configuración inicial del proyecto

---
+++ core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Catalogo(models.Model):
+    tipo_catalogo = models.CharField(max_length=50) # 'estamento', 'facultad_dependencia', 'rol', 'estado_solicitud'
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.tipo_catalogo} - {self.nombre}"
+    
+class Sede(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+    
+class DiaFestivoColombia(models.Model):
+    fecha_festivo = models.DateField(unique=True)
+    descripcion = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.fecha_festivo}: {self.descripcion}"
