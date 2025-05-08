b03571a0403d65adee59e9ca827cb4a71d76f954
Bryan Biojó <bryan.biojo@correounivalle.edu.co>
2025-05-07 23:08:24 -0500

    fix: Se crea serializer propio para 'ultimo_periodo'

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 
+class UltimoPeriodoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    fecha_inicio = serializers.DateField()
+    fecha_fin = serializers.DateField()
+    dias_totales = serializers.IntegerField()
+    dias_pendientes = serializers.IntegerField()
+    dias_disfrutados = serializers.IntegerField()
+
 class EstadoVacacionesSerializer(serializers.Serializer):
     antiguedad_dias = serializers.IntegerField()
     puede_solicitar = serializers.BooleanField()
     dias_pendientes_totales = serializers.IntegerField()
-
-    ultimo_periodo = serializers.DictField(
-        child=serializers.CharField(allow_null=True),
-        allow_null=True
-    )
+    ultimo_periodo = UltimoPeriodoSerializer(allow_null=True)
